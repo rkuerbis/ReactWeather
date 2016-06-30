6ac1467162c65c3f09cec56eeda81dd0a7261047
Ralph Kuerbis <ralph@kuerbis.ca>
2016-06-29 22:52:16 -0700

    Add CSS styles file app.css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -19,7 +19,7 @@ var {Link} = require('react-router');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>This is a weather application built on React. I have built this
          for The Complete React Web App Developer Course.
       </p>
